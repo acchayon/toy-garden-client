b41dc1069e42e4add5af23f5802c2b6fe7c52c93
acchayon <adhikaryac81@gmail.com>
2023-05-18 10:33:13 +0600

    added navbar

--- src/Page/Shared/Navbar/Navbar.jsx
+++ src/Page/Shared/Navbar/Navbar.jsx
@@ -2,8 +2,42 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is header</h2>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Home</a></li>
+                        <li tabIndex={0}>
+                            <a className="justify-between">
+                                All Toys
+                                
+                            </a>
+                            
+                        </li>
+                        <li><a>Blogs</a></li>
+                    </ul>
+                </div>
+                <img className='md:w-20 w-2/12  rounded-full' src="https://i.ibb.co/S5GV9GT/0e84360419c875fb2fda5739613309d2-removebg-preview.png" alt="" />
+                <a className="btn btn-ghost normal-case md:text-4xl font-bold">Toy Garden</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li tabIndex={0}>
+                        <a>
+                            All Toys
+                        </a>
+                       
+                    </li>
+                    <li><a>Blogs</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Login</a>
+            </div>
         </div>
     );
 };
